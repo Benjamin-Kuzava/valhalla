267caa2726aa817aac0754813351ebb2c0a48b23
benjamin kuzava <benjamin@kuzava.local>
2021-02-09 12:09:41 -0500

    -Added routing to Nav component -Added Layout component to Sign in, up, and out (Ben, Daniel, Damion, Vinny)

--- client/src/screens/SignIn/SignIn.jsx
+++ client/src/screens/SignIn/SignIn.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
 import "./SignIn.css";
 
 const SignIn = () => {
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <Layout>
+      <div>
+        <h1>SignIn</h1>
+      </div>
+    </Layout>
   );
 };
 
--- client/src/screens/SignOut/SignOut.jsx
+++ client/src/screens/SignOut/SignOut.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
 import "./SignOut.css";
 
 const SignOut = () => {
   return (
-    <div>
-      <h1>SignOut</h1>
-    </div>
+    <Layout>
+      <div>
+        <h1>SignOut</h1>
+      </div>
+    </Layout>
   );
 };
 
