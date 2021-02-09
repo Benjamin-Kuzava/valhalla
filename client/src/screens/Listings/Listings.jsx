c7f776f54255a62a17b6f304b0f35f4aa6dd957a
Daniel Ramirez <danielramirez@daniels-mbp.home>
2021-02-09 11:40:27 -0500

    made changes to listings.jsx

--- client/src/screens/Listings/Listings.jsx
+++ client/src/screens/Listings/Listings.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import "./Listings.css";
+import { getListings } from "../../services/listings";
+
 const Listings = () => {
+  const [allListings, setAllListings] = useState([]);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listings = await getListings();
+      setAllListings(listings);
+    };
+    fetchListings();
+  }, []);
   return (
     <Layout>
       <div>
