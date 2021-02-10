34fe8ebe4f9faff072371ee29cb57afef8e18525
Daniel Ramirez <danielramirez@daniels-mbp.home>
2021-02-10 14:42:58 -0500

    files changed: -home.jsx, .css -Silderdata.jsx

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -8,10 +8,27 @@ const Home = (props) => {
   return (
     <Layout user={props.user}>
       <div id="home-container">
-        <ImageSlider />
-        <h1>Home</h1>
+        <div className="slider-container">
+          <ImageSlider />
+        </div>
+        <div className="about-container">
+          <div className="about-info-container"></div>
+          <p>
+            we sell islands because why not? Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Neque, reprehenderit alias ullam non
+            perspiciatis fugit odio aliquid itaque sint dolor beatae sed?
+            Voluptatem dicta, consequuntur deleniti adipisci a dolores cumque.
+          </p>
+        </div>
+        <div className="branding-container">
+          <p>
+            branding info buy an island beacuse all the cool rich people are
+            doing it. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Enim ab ratione impedit atque itaque laudantium.
+          </p>
+          <div className="branding-info-container"></div>
+        </div>
       </div>
-      <div>{/* <ListingCards /> */}</div>
     </Layout>
   );
 };
