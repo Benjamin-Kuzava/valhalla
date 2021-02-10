09a8a10e87e11513bf54c0182032c93523505f56
Developer genius <developer_genius@protonmail.com>
2021-02-10 18:20:19 -0500

    -Layout the images -styled the rendered images -Working on gettting a limited about of data from api

--- client/src/screens/ListingDetail/ListingDetail.jsx
+++ client/src/screens/ListingDetail/ListingDetail.jsx
@@ -1,12 +1,37 @@
 import React from "react";
 import Layout from "../../components/shared/Layout/Layout";
+import { getListings } from "../../services/listings";
+import { useState } from 'react';
 import "./ListingDetail.css";
 
 const ListingDetail = (props) => {
+  const [allListings, setAllListings] = useState([]);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      const listings = await getListings();
+      setAllListings(listings);
+      listings.forEach((listing) =>{
+        console.log(listing)
+      })
+    };
+    fetchListings();
+  }, []);
+
   return (
     <Layout user={props.user}>
-      <div>
-        <h1>Detail</h1>
+       <div className="image-container">
+          
+          <h3>Recommended Listings</h3>
+
+        
+          {allListings.map((listing) => (
+            <div className="image-details">
+            <img src={listing.imgURL} />
+            <h3>{listing.name}</h3>
+            <h3>{listing.price}</h3>
+            </div>
+          ))};
       </div>
     </Layout>
   );
