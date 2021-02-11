727806785464ccd311ac89310d382cab490328fa
vinod kumar <vinod513kumar@gmail.com>
2021-02-11 09:17:05 -0500

    added Search Bar Search Bar functionality completed

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Search.css";
+import { useState } from "react";
 
-const Search = () => {
+
+const Search = (props) => {
+  const { handleChange, setQueriedListings } = props;
+  const [search, setSearch] = useState([])
+
   return (
     <div>
-      <h1>Search</h1>
+      <form className="search-form">
+        <input 
+          className="search-input"
+          type="text"
+          name="searchBar"
+          id="searchbar"
+          placeholder="search"
+          value={search}
+          onChange={handleChange}
+          />
+      </form> 
     </div>
   );
 };
