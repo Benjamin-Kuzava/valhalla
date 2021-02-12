7d60f2caf47826f1156b9e1cf74f758edfc9b8da
Daniel Ramirez <danielramirez@daniels-mbp.home>
2021-02-12 09:18:59 -0500

    made changes to create listing form added bkgrd, form, css modified red block on about page

--- client/src/screens/ListingCreate/ListingCreate.jsx
+++ client/src/screens/ListingCreate/ListingCreate.jsx
@@ -1,24 +1,93 @@
-import React, {useState} from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import "./ListingCreate.css";
-
+import React, { useState } from "react";
+import { createListing } from "../../services/listings";
+import { Redirect } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const ListingCreate = (props) => {
-  const [listing, setListing] = useState(
-    {
-      name: "",
-      imgURL: "",
-      description: "",
-      price: "",
-    })
-  const [isCreated, setIsCreated] = useState(false);
+  const [listing, setListing] = useState({
+    title: "",
+    description: "",
+    imgURL: "",
+    price: "",
+  });
+
+  const [isCreated, setCreated] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setListing({
+      ...listing,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const created = await createListing(listing);
+    setCreated({ created });
+  };
+
+  if (isCreated) {
+    return <Redirect to={`/listings`} />;
+  }
+
+  const { name, price, description, author } = listing;
   return (
-    <Layout user={props.user}>
-      <div>
-        <h1>Create</h1>
+    <Layout>
+      <div className="background">
+        <div className="create-page"></div>
+        <div className="create-container">
+          <h3>Create A Listing</h3>
+          <form onSubmit={handleSubmit}>
+            <label>Name of Property</label>
+            <input
+              className="create-input"
+              required
+              type="text"
+              name="name"
+              value={name}
+              placeholder="Enter Name of Property"
+              onChange={handleChange}
+            />
+            <label>Price</label>
+            <input
+              className="create-input"
+              required
+              type="text"
+              name="price"
+              value={price}
+              placeholder="Enter Price in USD"
+              onChange={handleChange}
+            />
+            <label>Description</label>
+            <input
+              className="create-input"
+              required
+              name="description"
+              value={description}
+              type="text"
+              placeholder="Description"
+              onChange={handleChange}
+            />
+            <label>Author</label>
+            <input
+              className="create-input"
+              required
+              name="author"
+              value={author}
+              type="text"
+              placeholder="Author"
+              onChange={handleChange}
+            />
+            <button id="sign-in-button" type="submit">
+              Create Listing
+            </button>
+          </form>
+        </div>
       </div>
     </Layout>
   );
 };
-
 export default ListingCreate;
