a00575c1bc9d7a3749213407aa2f8f2119cc2919
vinod kumar <vinod513kumar@gmail.com>
2021-02-16 16:24:29 -0500

    added content & css style to Footer component

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <h1>Valhalla</h1>
       </Link>
       <div className="footer-text">
-        <h4><bold>Created Team :</bold></h4>
+        <h4>Created Team :</h4>
         <img src="https://image.shutterstock.com/image-vector/github-vector-icon-apps-web-260nw-1835154331.jpg" width="20px" height="20px" />
          <a href="https://github.com/dantheman491" target="blank">Daniel Ramirez</a>
         <a href="https://github.com/vinod-git12" target="blank">Vinod Kumar</a>
