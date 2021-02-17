c79e3e2cf9370c13ce017fd09817a9bae20a103b
Daniel Ramirez <danielramirez@daniels-mbp.home>
2021-02-17 14:18:56 -0500

    added css changes to about screen

--- client/src/components/shared/Layout/Layout.jsx
+++ client/src/components/shared/Layout/Layout.jsx
@@ -7,9 +7,9 @@ const Layout = (props) => {
   return (
     <div className="layout-children">
       <div className="layout-content">
-      <Nav user={props.user} />
-      {/* screens go below */}
-      {props.children}
+        <Nav user={props.user} />
+        {/* screens go below */}
+        {props.children}
       </div>
       <Footer />
     </div>
