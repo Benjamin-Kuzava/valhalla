3e4492b8d8fbfcb5878aee14e9f4ea569648724b
vinod kumar <vinod513kumar@gmail.com>
2021-02-17 16:59:23 -0500

    added media queries to footer page

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -10,10 +10,12 @@ const Footer = () => {
       </Link>
       <div className="footer-text">
         <h4 className="footer-h4">Created Team : </h4>
-         <a className="footer-a" href="https://github.com/dantheman491" target="blank"><i class="fab fa-github"></i>Daniel Ramirez</a>
-        <a className="footer-a" href="https://github.com/vinod-git12" target="blank"><i class="fab fa-github"></i>Vinod Kumar</a>
-        <a className="footer-a" href="https://github.com/Benjamin-Kuzava/" target="blank"><i class="fab fa-github"></i>Benjamin Kuzava</a>
-        <a className="footer-a" href="https://github.com/DamionStewart0" target="blank"><i class="fab fa-github"></i> Damion Stewart</a>
+        <div>
+          <a className="footer-a" href="https://github.com/dantheman491" target="blank"><i class="fab fa-github"></i>  &nbsp; Daniel Ramirez</a>
+          </div>
+        <a className="footer-a" href="https://github.com/vinod-git12" target="blank"><i class="fab fa-github"></i>  &nbsp; Vinod Kumar</a>
+        <a className="footer-a" href="https://github.com/Benjamin-Kuzava/" target="blank"><i class="fab fa-github"></i>  &nbsp; Benjamin Kuzava</a>
+        <a className="footer-a" href="https://github.com/DamionStewart0" target="blank"><i class="fab fa-github"></i>  &nbsp; Damion Stewart</a>
       </div>
       </div>
   )};
